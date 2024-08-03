b72259e78cde41312a4128e18146703db7fd6ce3
Vítor <vitor.ass@hotmail.com>
2024-08-02 21:15:43 -0300

    deleted:    meu-primeiro-projeto-java/src/edu/Vitor/java_basico/App.java 	new file:   meu-primeiro-projeto-java/src/edu/Vitor/java_basico/Operadores.java

--- meu-primeiro-projeto-java/src/edu/Vitor/java_basico/App.java
+++
@@ -1,6 +0,0 @@
-package edu.Vitor.java_basico;
-public class App {
-    public static void main(String[] args)  {
-        System.out.println("Boas vindas turma dio");
-    }
-}
---
+++ meu-primeiro-projeto-java/src/edu/Vitor/java_basico/Operadores.java
@@ -0,0 +1,16 @@
+package edu.Vitor.java_basico;
+public class Operadores {
+    public static void main(String[] args)  {
+        int a = 5, b = 6 ;
+        String resultado = a == b ? "Verdadeiro" : "Falso";
+    //     Operador ternário é uma forma simplificada de fazer um if else
+    //     O operador tenário é a mesma coisa disso: 
+    //     if(a == b) {
+    //         resultado = "Verdadeiro";
+    //     } else {
+    //         resultado = "Falso";
+    //     }
+
+        System.out.println(resultado);
+    }
+}
