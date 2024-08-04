b5cb363782de4a9a703a5be05e4217ee2dfc5125
Vítor <vitor.ass@hotmail.com>
2024-08-03 21:32:52 -0300

    new file:   java_basico/src/Transformacao.java

---
+++ java_basico/src/Transformacao.java
@@ -0,0 +1,12 @@
+public class Transformacao {
+   //O método Integer.valueOf() é utilizado para converter 
+   // um texto em um número inteiro.
+        public static void main(String[] args) {
+            String numeroEmTexto = "123";
+            Integer numero = Integer.valueOf(numeroEmTexto);
+            
+            System.out.println("Número: " + numero);
+            System.out.println("Texto: " + numeroEmTexto);
+        }
+    
+}
