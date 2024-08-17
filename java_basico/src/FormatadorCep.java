0bab9bc18e3c2c0c2d11a00c95240c2cccbd8812
Vítor <vitor.ass@hotmail.com>
2024-08-16 23:48:53 -0300

    new file:   java_basico/src/CepInvalidoException.java 	new file:   java_basico/src/FormatadorCep.java 	new file:   java_basico/src/InputMismatchExpection.java 	new file:   java_basico/src/TryCatch.java

---
+++ java_basico/src/FormatadorCep.java
@@ -0,0 +1,17 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try{
+           String cepFormatado = formatarCep("23765064");
+           System.out.println(cepFormatado);
+        }catch(CepInvalidoException e){
+            System.err.println("O CEP não corresonde com as regras de negocio");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          return "23.765-064";
+    }
+    
+}
